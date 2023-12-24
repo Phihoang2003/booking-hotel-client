bb2b0de672fd3f67dddcd2dfe05c9dfca2ad1953
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-12-24 14:18:45 +0700

    update

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -9,26 +9,27 @@ const FeaturedProperties = () => {
       {loading ? (
         "Loading please"
       ) : (
-        <>
-          {data?.map((item) => {
-            return (
-              <div className="fpItem" key={item._id}>
-                <img src={item.photos[0]} alt="" className="fpImg" />
-                <span className="fpName">{item.name}</span>
-                <span className="fpCity">{item.city}</span>
-                <span className="fpPrice">
-                  Staring from {item.cheapestPrice}
-                </span>
-                {item?.rating && (
-                  <div className="fpRating">
-                    <button>{item.rating}</button>
-                    <span>Excellent</span>
-                  </div>
-                )}
-              </div>
-            );
-          })}
-        </>
+        <></>
+        // <>
+        //   {data?.map((item) => {
+        //     return (
+        //       <div className="fpItem" key={item._id}>
+        //         <img src={item.photos[0]} alt="" className="fpImg" />
+        //         <span className="fpName">{item.name}</span>
+        //         <span className="fpCity">{item.city}</span>
+        //         <span className="fpPrice">
+        //           Staring from {item.cheapestPrice}
+        //         </span>
+        //         {item?.rating && (
+        //           <div className="fpRating">
+        //             <button>{item.rating}</button>
+        //             <span>Excellent</span>
+        //           </div>
+        //         )}
+        //       </div>
+        //     );
+        //   })}
+        // </>
       )}
     </div>
   );
